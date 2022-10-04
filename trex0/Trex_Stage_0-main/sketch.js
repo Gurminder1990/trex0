931853d8da51f78fc76f26793a839de077800f17
Gurminder1990 <75359289+Gurminder1990@users.noreply.github.com>
2022-10-04 12:34:46 +0530

    Add files via upload

---
+++ trex0/Trex_Stage_0-main/sketch.js
@@ -0,0 +1,22 @@
+
+var trex ,trex_running;
+
+
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,600)
+  
+  //create a trex sprite
+ trex=createSprite()
+
+}
+
+function draw(){
+  background("white")
+  
+
+}
